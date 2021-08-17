564f0356a9ce9a89a98ca1793f0421f9ae44399d
Nombuso <38411152+Insitepass@users.noreply.github.com>
2021-08-18 00:31:11 +0200

    Main.dart file
    This is the main dart file for the flutter project.

---
+++ main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:survery_application/pages/Intro.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  static const MaterialColor primaryBlack = MaterialColor(
+    _blackPrimaryValue,
+    <int, Color>{
+      50: Color(0xFF000000),
+      100: Color(0xFF000000),
+      200: Color(0xFF000000),
+      300: Color(0xFF000000),
+      400: Color(0xFF000000),
+      500: Color(_blackPrimaryValue),
+      600: Color(0xFF000000),
+      700: Color(0xFF000000),
+      800: Color(0xFF000000),
+      900: Color(0xFF000000),
+    },
+  );
+  static const int _blackPrimaryValue = 0xFF000000;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: primaryBlack,
+      ),
+      home: IntroPages()
+    );
+  }
+}
+
