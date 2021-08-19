074237d0ce6e920232e2fcf9032c07e6baf1e3b7
Nombuso <38411152+Insitepass@users.noreply.github.com>
2021-08-19 04:34:28 +0200

    reset password

---
+++ survey_application/lib/resetpassword.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class ResetPassword extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _State();
+}
+
+class _State extends State<ResetPassword> {
+  TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+            padding: EdgeInsets.all(20),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Survey App',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
+                      labelText: 'Email address',
+                    ),
+                  ),
+                ),
+                Container(
+                    height: 40,
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.black,
+                        textStyle: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0)),
+                      ),
+                      child: Text('Reset'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    )),
+              ],
+            )));
+  }
+}
