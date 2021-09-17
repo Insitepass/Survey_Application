51009a651a74dc259cd061b0bcb6fc3f492faf9f
Nombuso <38411152+Insitepass@users.noreply.github.com>
2021-09-17 19:52:17 +0200

    updated main

--- main.dart
+++ main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:survery_application/pages/Intro.dart';
+import 'package:survey_application/login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -25,25 +28,13 @@ class MyApp extends StatelessWidget {
   );
   static const int _blackPrimaryValue = 0xFF000000;
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: primaryBlack,
       ),
-      home: IntroPages()
+      home: LoginPage(),
     );
   }
 }
-
